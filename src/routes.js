e5e0ee608a6c765fff33188754f159045313e73c
shakil Hossen <shakilhossen307@gmail.com>
2023-03-29 15:07:47 +0600

    Teacher Form Control

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import AddBlog from './views/blog/AddBlog'
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
+const Teacher = React.lazy(() => import('./views/teacher'))
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/addblog', name: 'Add Blog', element: AddBlog },
+  { path: '/teacher', name: 'Add Teacher', element: Teacher },
 ]
 export default routes
