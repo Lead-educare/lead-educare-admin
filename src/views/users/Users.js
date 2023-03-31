aefc31c854381c468e5a5d239b3163cfc2550ac5
sohan653 <sohanurl653@gmail.com>
2023-03-31 19:16:45 +0600

    users page updated

--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -1,38 +1,57 @@
 import React from 'react';
-import { AiFillDelete,AiFillEdit } from "react-icons/ai";
+import {
+  CBadge,
+  CButton,
+  CFormInput,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow
+} from "@coreui/react";
+import {FiEdit} from "react-icons/fi";
+import {RiDeleteBin5Fill} from "react-icons/ri";
 const users=[
   {
     name:"sohan",
+    email:"sohan@gmail.com",
     role:'admin',
     gems:12
   },
   {
     name:"shakil",
+    email:"shakil@gmail.com",
     role:'student',
     gems:15
   },
   {
     name:"dip",
+    email:"dip@gmail.com",
     role:'student',
     gems:12
   },
   {
     name:"afnan",
+    email:"sohan@gmail.com",
     role:'teacher',
     gems:14
   },
   {
     name:"jony",
+    email:"sohan@gmail.com",
     role:'admin',
     gems:12
   },
   {
     name:"alamgir",
+    email:"sohan@gmail.com",
     role:'student',
     gems:12
   },
   {
     name:"parvej",
+    email:"sohan@gmail.com",
     role:'admin',
     gems:12
   },
@@ -40,36 +59,71 @@ const users=[
 const Users = () => {
   return (
     <div>
-      <div>
-        <table className="table">
-          <thead style={{backgroundColor:'#dfe6e9'}} >
-          <tr>
-            <th className={'text-center'} scope="col">id</th>
-            <th className={'text-center'} scope="col">Name</th>
-            <th className={'text-center'} scope="col">Role</th>
-            <th className={'text-center'} scope="col">Gems</th>
-            <th className={'text-center'} scope="col">Action</th>
-          </tr>
-          </thead>
-          <tbody>
+      <div className="card border-0 shadow-sm">
+        <div className="card-body">
+          <h6 className="card-title">All Blogs</h6>
+          <div className="my-3 d-flex">
+            <CFormInput
+              placeholder="Search by title"
+              className="w-25"
+            />
+          </div>
+          <CTable>
+            <CTableHead>
+              <CTableRow style={{backgroundColor:'#dfe6e9'}}>
+                <CTableHeaderCell className="py-3" scope="col" style={{ fontSize: '14px' }}>
+                  Id
+                </CTableHeaderCell>
+                <CTableHeaderCell className="py-3" scope="col" style={{ fontSize: '14px' }}>
+                  Name
+                </CTableHeaderCell>
+                <CTableHeaderCell className="py-3" scope="col" style={{ fontSize: '14px' }}>
+                  Email
+                </CTableHeaderCell>
+                <CTableHeaderCell className="py-3" scope="col" style={{ fontSize: '14px' }}>
+                  Role
+                </CTableHeaderCell>
+                <CTableHeaderCell className="py-3" scope="col" style={{ fontSize: '14px' }}>
+                  Gems
+                </CTableHeaderCell>
+                <CTableHeaderCell className="py-3 text-center" scope="col" style={{ fontSize: '14px' }}>
+                  Action
+                </CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
 
-          {
-            users.map((user,key) => {
-              return <tr className={'my-2'}>
-                <td className={'text-center'}>{key + 1}</td>
-                <td className={'text-center'}>{user.name}</td>
-                <td className={'text-center'} style={{color:`${ user.role === 'admin' ? '#6c5ce7':"" || user.role === 'student' ? '#0984e3':"" || user.role === 'teacher' ? '#00cec9':"" }`}}>{user.role}</td>
-                <td className={'text-center'}>{user.gems}</td>
-                <td className={'d-flex justify-content-center '}>
-                  <button className={'btn btn-secondary me-2'}> <AiFillEdit/> </button>
-                  <button className={'btn btn-danger'}><AiFillDelete/></button>
-                </td>
-              </tr>
-            })
-          }
+            <CTableBody>
+              {users.map((user,key)=>{
+                return <CTableRow>
+                  <CTableDataCell style={{ fontSize: '14px', color: '#8E98AA' }}>{key+1}</CTableDataCell>
+                  <CTableDataCell style={{ fontSize: '14px', color: '#57606f' }}> {user.name}</CTableDataCell>
+                  <CTableDataCell style={{ fontSize: '14px', color: '#57606f' }}>{user.email}</CTableDataCell>
+                  <CTableDataCell style={{ fontSize: '14px', color: '#57606f' }}> <CBadge style={{ backgroundColor:'#34495e'}}>{user.role}</CBadge> </CTableDataCell>
+                  <CTableDataCell style={{ fontSize: '14px', color: '#57606f' }}> {user.gems}</CTableDataCell>
+                  <CTableDataCell className="d-flex align-items-center  justify-content-center w-full">
 
-          </tbody>
-        </table>
+                    <CButton
+                      // onClick={() => onDelete(blog?.blogID)}
+                      className=" border-0 cursor-pointer me-2 delete_btn_hover"
+                      style={{ color: '#ecf0f1',backgroundColor:"#20bf6b" }}
+                    >
+                      <FiEdit />
+                    </CButton>
+                    <CButton
+                      // onClick={() => onDelete(blog?.blogID)}
+                      className=" border-0 cursor-pointer delete_btn_hover"
+                      style={{ color: '#ecf0f1',backgroundColor:"red" }}
+                    >
+                      <RiDeleteBin5Fill />
+                    </CButton>
+                  </CTableDataCell>
+                </CTableRow>
+              })}
+
+            </CTableBody>
+          </CTable>
+
+        </div>
       </div>
     </div>
   );
